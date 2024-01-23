e071dbad85306a7b04dc0b2dc2cd33cf470c7732
davesplace <drtalley85@gmail.com>
2024-01-23 15:03:51 -0600

    updated folders

--- demo4/top.v
+++ demo4/top.v
@@ -1,7 +1,6 @@
-`default_nettype none
+//`default_nettype none
 
 module top;
-
 reg t1;
 reg t2;
 reg t3, t4;
@@ -27,10 +26,43 @@ initial begin
     c[0] = 8'bxx11_0x0x; // underscore is ignored in verilog for reading
     c[1] = 8'b1111_0000;
     $display("a = %b, b = %b, c[0] = %b, c[1] = %b", a, b , c[0], c[1]);
-    #50 b = a;  //This will only capture the bits of a that match the size of b
+    //#50 b = a;  //This will only capture the bits of a that match the size of b
     //any higher order bits will be truncated
-    $display("a = %b, b = %b, c[0] = %b, c[1] = %b", a, b , c[0], c[1]);
-
+   // $display("a = %b, b = %b, c[0] = %b, c[1] = %b", a, b , c[0], c[1]);
 end
 
+reg clk, reset;
+initial begin
+    reset = 0;
+    clk = 0;
+end
+always begin
+    #5 clk = ~clk; //this will toggle the clock signal every 5 time units
+end
+
+example_module U1 (clk, reset);
+initial begin
+    #500 $finish; //Finish is a system task that ends the simulation
+end
+endmodule
+
+module example_module(input clk, input reset);
+reg [7:0] count = 0; // little bit-wise endian
+reg [0:7] count2 = 0; // bit bit-wise endian
+reg enable = 1;
+//assign data2 = count
+assign data = count;
+assign data2 = count2;
+
+always @(posedge clk or posedge reset) begin
+    if (reset) begin
+        count <= 0;
+        enable <= 0;
+    end
+    else if (enable) begin
+        count <= count + 1;
+        count2 <= count2 + 1;
+    end
+    $display("data = %b data2 = %b count = %b count2 = %b", data, data2, count, count2);
+end
 endmodule
